f1b78e61f81209e6a580d97ed129f36d263e358a
Louisececilia <louise_lind@outlook.com>
2021-11-12 10:11:33 +0100

    steg1

--- lib/main.dart
+++ lib/main.dart
@@ -6,110 +6,153 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TIG169 TODO',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'TIG169 TODO'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        centerTitle: true,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SecondView()));
+            },
+          ),
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(height: 30),
+            _checkbox('Write a book'),
+            _divide(),
+            _checkbox('Do homework'),
+            _divide(),
+            _checkedcheckbox('Tidy room'),
+            _divide(),
+            _checkbox('Watch TV'),
+            _divide(),
+            _checkbox('Nap'),
+            _divide(),
+            _checkbox('Shop groceries'),
+            _divide(),
+            _checkbox('Have fun'),
+            _divide(),
+            _checkbox('Meditate'),
+            _divide(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton:
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
     );
+  }
+
+  Widget _checkbox(String text) {
+    return Row(
+      children: [
+        Checkbox(
+          value: false,
+          onChanged: (val) {},
+        ),
+        Text(text, style: const TextStyle(fontSize: 21)),
+        const Spacer(),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.clear)),
+      ],
+    );
+  }
+
+  Widget _checkedcheckbox(String text) {
+    return Row(
+      children: [
+        Checkbox(
+          value: true,
+          onChanged: (val) {},
+        ),
+        Text(text,
+            style: const TextStyle(
+                decoration: TextDecoration.lineThrough, fontSize: 21)),
+        const Spacer(),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.clear)),
+      ],
+    );
+  }
+
+  Widget _divide() {
+    return const Divider(
+      height: 15,
+      thickness: 1,
+    );
+  }
+}
+
+class SecondView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('TIG169 TODO'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 20.0,
+              ),
+              _inputBox(),
+              _button(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _inputBox() {
+    return Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(5),
+        margin: const EdgeInsets.all(20),
+        child: const TextField(
+            decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black, width: 2.0)),
+          labelText: 'What are you going to do?',
+        )));
+  }
+
+  Widget _button() {
+    return TextButton(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+        ),
+        onPressed: () {},
+        child: const Text(
+          '+ ADD',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ));
   }
 }
